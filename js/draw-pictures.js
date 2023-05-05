1e3844cc2d462e177ab11bbd0b6309691538488a
IlyaFonichev <il-shturm@mail.ru>
2023-05-05 19:53:58 +0300

    module7-task1

---
+++ js/draw-pictures.js
@@ -0,0 +1,21 @@
+import { generatePhotos } from './data.js';
+
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const picturesContainer = document.querySelector('.pictures');
+const photos = generatePhotos();
+
+const renderPhotos = () => {
+  const fragment = document.createDocumentFragment();
+
+  photos.forEach((photo) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = photo.url;
+    pictureElement.querySelector('.picture__likes').textContent = photo.likes;
+    pictureElement.querySelector('.picture__comments').textContent = photo.comments;
+    fragment.appendChild(pictureElement);
+  });
+
+  picturesContainer.appendChild(fragment);
+};
+
+export { renderPhotos };
